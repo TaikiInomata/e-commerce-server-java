ffcb024d773e345a1c2bd1455da32e681a059da3
Novi <thanhdungng04@gmail.com>
2025-09-15 14:36:44 +0700

    first commit

---
+++ iam-service/iam-start/src/main/java/com/be/IamStart.java
@@ -0,0 +1,12 @@
+package com.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamStart {
+    public static void main(String[] args) {
+        SpringApplication.run(IamStart.class, args);
+        // System.out.println("Hello world!");
+    }
+}
